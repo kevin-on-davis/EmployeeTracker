b787bf5f91c84a60b1e6ee5b3ae2655a693e6332
kevin-on-davis <kevin.on.davis@gmail.com>
2019-11-24 14:37:09 -0500

    updating EmployereTracker for deploy to heroku

--- server.js
+++ server.js
@@ -55,9 +55,8 @@ async function employeeTracker() {
       name: "menu_option",
       choices: [
         "Maintain Department Information",
-        "Maintain Employee Information"
-        // ,
-        // "Terminator - end data entry"
+        "Maintain Employee Information",
+        "Terminator - end data entry"
       ]
     }
   ]);
@@ -65,8 +64,8 @@ async function employeeTracker() {
     let mngrObject = await departmentInfo();
   } else if (part1.menu_option == "Maintain Employee Information") {
     let engnrObject = await employeeMaintenance();
-    // } else if (part1.menu_option == "Terminator - end data entry") {
-    //   end_condtn = true;
+  } else if (part1.menu_option == "Terminator - end data entry") {
+    end_condtn = true;
   }
 }
 
